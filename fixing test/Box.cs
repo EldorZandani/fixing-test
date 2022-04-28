c8b970236ac23ef211afdd045eb846a5dfde5cd0
EldorZandani <fox18966@gmail.com>
2022-04-28 15:30:30 +0300

    how to answer the test question

---
+++ fixing test/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fixing_test
+{
+    class Box
+    {
+        private string name;
+        private int cal;
+        private int fat;
+
+        public Box(string name, int cal, int fat)
+        {
+            this.name = name;
+            this.cal = cal;
+            this.fat = fat;
+        }
+
+        public bool IsHelthy()
+        {
+            return this.cal < 130 && this.fat < 5;
+        }
+        public int GetCal()
+        {
+            return this.cal;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ fixing test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fixing_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int cal;
+            int fat;
+            int count = 0;
+            int maxCal = 0;
+            string nameOfMax = "";
+            Console.WriteLine("please enter the name of the product");
+            name = Console.ReadLine();
+            while (name != "stop")
+            {
+                Console.WriteLine("please enter calories");
+                cal = int.Parse(Console.ReadLine());
+                Console.WriteLine("please enter fat");
+                fat = int.Parse(Console.ReadLine());
+                Box c = new Box(name, cal, fat);
+                if (c.IsHelthy())
+                    count++;
+                if (c.GetCal()>maxCal)
+                {
+                    maxCal = c.GetCal();
+                    nameOfMax = c.GetName();
+                }
+                Console.WriteLine("please enter the name of the product");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("Number of products that not bad for health" + count);
+            Console.WriteLine(nameOfMax+ "has maximum calories");
+        }
+    }
+}
